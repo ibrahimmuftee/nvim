defcbe319b8c17b9b2383ecb15d2c2b870821380
Ibrahim Muftee <ibrahim@ibrahimmuftee.net>
2024-02-04 16:51:57 -0600

    Explicitly configuring every option

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,16 +18,44 @@
 return {
 
   "ellisonleao/gruvbox.nvim",
-  opts = {
-    options = {
-      transparent_bg = true;
-    },
-  },
-  config = function(_, opts)
+  -- opts = {
+  --   options = {
+  --     transparent_mode = true,
+  --   },
+  -- },
+  -- config = function()
+  --   vim.opt.termguicolors = true
+  --   vim.opt.background = "dark"
+  --
+  --   vim.cmd.colorscheme("gruvbox")
+  -- end,
+  init = function()
+    require("gruvbox").setup({
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+
     vim.opt.termguicolors = true
     vim.opt.background = "dark"
 
     vim.cmd.colorscheme("gruvbox")
   end,
-
 }
