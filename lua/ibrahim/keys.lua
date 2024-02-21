e2cdd541664fb43dcc3c7b526a3031bcb008fdba
Ibrahim Muftee <ibrahim@ibrahimmuftee.net>
2024-02-21 10:16:55 -0600

    feat: enable LSP keybindings again cuz i know how they work now

--- lua/ibrahim/keys.lua
+++ lua/ibrahim/keys.lua
@@ -66,3 +66,77 @@ map({ "n", "v" }, "<leader>cd", '"+d', { desc = "Clipboard delete" })
 map({ "n", "v" }, "<leader>cp", '"+p', { desc = "Clipboard paste" })
 map({ "n", "v" }, "<leader>cy", '"+y', { desc = "Clipboard yank" })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("JoshuaLspConfig", {}),
+    callback = function(evt)
+        local function lsp_map(mode, lhs, rhs, desc)
+            local opts = { buffer = evt.buf, desc = "LSP: " .. (desc or "") }
+            map(mode, lhs, rhs, opts)
+        end
+
+        local has_telescope, _ = pcall(require, "telescope")
+        local function lsp_telescope_map(mode, lhs, with_telescope_rhs, no_telescope_rhs, desc)
+            if has_telescope then
+                desc = desc .. " (Telescope)"
+                lsp_map(mode, lhs, with_telescope_rhs, desc)
+            else
+                lsp_map(mode, lhs, no_telescope_rhs, desc)
+            end
+        end
+        local function tele_builtin(name, opts)
+            return function()
+                require("telescope.builtin")[name](opts)
+            end
+        end
+
+        -- Navigation
+        -- Some of these get overridden by Telescope later
+        lsp_map("n", "gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        lsp_map("n", "gt", vim.lsp.buf.type_definition, "[G]oto [t]ype Definition")
+        lsp_map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        -- lsp_map("n", "gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+        lsp_map("n", "gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        lsp_map("n", "<leader>sd", vim.lsp.buf.document_symbol, "[S]earch [D]ocument symbol names")
+        lsp_map("n", "<leader>sn", vim.lsp.buf.workspace_symbol, "[S]earch [D]ocument symbol names")
+
+        lsp_telescope_map("n", "gr", tele_builtin("lsp_references"), vim.lsp.buf.references, "Go to references")
+        lsp_telescope_map(
+            "n",
+            "<leader>sd",
+            tele_builtin("lsp_document_symbols"),
+            vim.lsp.buf.document_symbol,
+            "Search document symbols"
+        )
+        lsp_telescope_map(
+            "n",
+            "<leader>sn",
+            tele_builtin("lsp_dynamic_workspace_symbols"),
+            vim.lsp.buf.workspace_symbol,
+            "Search workspace symbols"
+        )
+
+        -- Code changes
+        lsp_map("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
+        lsp_map("n", "<leader>ca", vim.lsp.buf.code_action, "Code actions")
+
+        -- Diagnostics
+        lsp_map("n", "<leader>ds", vim.diagnostic.open_float, "[s]how [d]iagnostic window")
+        lsp_map("n", "<leader>dn", function()
+            vim.diagnostic.goto_next({ float = true })
+        end, "Go to [n]ext [d]iagnostic")
+        lsp_map("n", "<leader>dp", function()
+            vim.diagnostic.goto_prev({ float = true })
+        end, "Go to [p]revious [d]iagnostic")
+
+        -- See `:help K` for why this keymap
+        lsp_map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+        lsp_map("n", "<C-S-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+        -- Workspace
+        lsp_map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+        lsp_map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+        lsp_map("n", "<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, "[W]orkspace [L]ist Folders")
+    end,
+})
